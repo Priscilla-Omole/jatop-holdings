5426df5a206e341168796ec8cbc4af8bb498002c
Priscilla-Omole <priscilla.omole@termii.com>
2023-04-26 04:00:46 +0100

    Initial commit

---
+++ src/main/java/com/example/jatopholdings/JatopHoldingsApplication.java
@@ -0,0 +1,13 @@
+package com.example.jatopholdings;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JatopHoldingsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JatopHoldingsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/jatopholdings/JatopHoldingsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jatopholdings;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JatopHoldingsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
